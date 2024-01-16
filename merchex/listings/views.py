0323f93997e2b6554e42df87b92e1b502af1dddf
GameWizzard2 <cbarfieldga@gmail.com>
2024-01-15 22:18:50 -0500

    Added views within the listings folder for webpage views. Added url pathing so web pages can be viewed

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    # creates a view
+    return HttpResponse('<h1>Hello Django</h1>')
+
+def about(request):
+    """ 
+    Returns the about page 
+    """
+    return HttpResponse('<h1>About us</h1> <p>We love merch!</p>')
+
+def contact(request):
+    """ 
+    Returns the contact page 
+    """
+    return HttpResponse('<h1>Contact</h1>' +
+                        '<p>Phone:000-000-0000<br>Email:Placeholder</p>')
+
+def merch(request):
+    """ 
+    Returns the merch page 
+    """
+    return HttpResponse('<h1>Work in progress</h1>')
