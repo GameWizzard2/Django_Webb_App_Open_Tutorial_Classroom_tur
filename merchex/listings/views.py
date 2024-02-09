2c15742b57277005241aa88da85778b1f2d0051d
GameWizzard2 <cbarfieldga@gmail.com>
2024-02-08 21:42:14 -0500

    Begining Separte app logic from pres

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
 
 def hello(request):
     # creates a view
-    return HttpResponse('<h1>Hello Django</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+                        <h1>Hello Django</h1>
+                        <p> A list of bands:<p>
+                        <ul>
+                            <li>{bands[0].name}</li>
+                            <li>{bands[1].name}</li>
+                            <li>{bands[2].name}</li>
+                            <li>{bands[3].name}</li>
+                            <li>{bands[4].name}</li>
+                        """)
 
 def about(request):
     """ 
